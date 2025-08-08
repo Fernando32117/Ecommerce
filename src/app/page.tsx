d7f9a53f23ccf55287b6e912c6ec222aedfc809f
franciscofernandouitec <franciscofernandouitec@gmail.com>
2025-08-08 01:46:37 -0300

    feat: Login com o Google e Criação da tela inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,64 @@
-"use client";
-import React from "react";
+import { desc } from "drizzle-orm";
+import Image from "next/image";
 
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
+import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
+import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
-export default function Home() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {
+      variants: true,
+    },
+  });
+  const categories = await db.query.categoryTable.findMany({});
+
   return (
-    <div>
-      <Button>Butttao</Button>
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border shadow-sm"
-        captionLayout="dropdown"
-      />
-    </div>
+    <>
+      <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={newlyCreatedProducts} title="Novos produtos" />
+        <Footer />
+      </div>
+    </>
   );
-}
+};
+
+export default Home;
