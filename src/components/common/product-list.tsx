173771c0db433edf89392f6e6c649756cf9774dc
franciscofernandouitec <franciscofernandouitec@gmail.com>
2025-08-15 16:24:14 -0300

    feat: Add carousel and integrate carousel component in product list

--- src/components/common/product-list.tsx
+++ src/components/common/product-list.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import { productTable, productVariantTable } from "@/db/schema";
 
 import ProductItem from "./product-item";
@@ -15,10 +22,21 @@ const ProductList = ({ title, products }: ProductListProps) => {
   return (
     <div className="space-y-6">
       <h3 className="px-5 font-semibold md:text-lg">{title}</h3>
-      <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
-        {products.map((product) => (
-          <ProductItem key={product.id} product={product} />
-        ))}
+      <div className="px-5">
+        <Carousel>
+          <CarouselContent>
+            {products.map((product) => (
+              <CarouselItem
+                key={product.id}
+                className="basis-3/4 md:basis-1/3 lg:basis-1/5"
+              >
+                <ProductItem product={product} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </div>
   );
