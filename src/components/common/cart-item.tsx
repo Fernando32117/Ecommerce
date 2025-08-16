9edb4e2c7834014a12d43669725e729375bafe5e
franciscofernandouitec <franciscofernandouitec@gmail.com>
2025-08-16 03:00:03 -0300

    feat: Inclusãodo do Dialog para confirmação de exclusão para itens do carrinho

--- src/components/common/cart-item.tsx
+++ src/components/common/cart-item.tsx
@@ -1,5 +1,6 @@
 import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 import { toast } from "sonner";
 
 import { formatCentsToBRL } from "@/helpers/money";
@@ -8,6 +9,14 @@ import { useIncreaseCartProduct } from "@/hooks/mutations/use-increase-cart-prod
 import { useRemoveProductFromCart } from "@/hooks/mutations/use-remove-product-from-cart";
 
 import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
 
 interface CartItemProps {
   id: string;
@@ -28,19 +37,27 @@ const CartItem = ({
   productVariantPriceInCents,
   quantity,
 }: CartItemProps) => {
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
   const removeProductFromCartMutation = useRemoveProductFromCart(id);
   const decreaseCartProductQuantityMutation = useDecreaseCartProduct(id);
   const increaseCartProductQuantityMutation =
     useIncreaseCartProduct(productVariantId);
-  const handleDeleteClick = () => {
+
+  const handleConfirmDelete = () => {
     removeProductFromCartMutation.mutate(undefined, {
       onSuccess: () => {
         toast.success("Produto removido do carrinho.");
+        setIsDeleteDialogOpen(false);
       },
       onError: () => {
         toast.error("Erro ao remover produto do carrinho.");
+        setIsDeleteDialogOpen(false);
       },
     });
+  };
+
+  const handleDeleteClick = () => {
+    setIsDeleteDialogOpen(true);
   };
   const handleDecreaseQuantityClick = () => {
     decreaseCartProductQuantityMutation.mutate(undefined, {
@@ -57,48 +74,85 @@ const CartItem = ({
     });
   };
   return (
-    <div className="flex items-center justify-between">
-      <div className="flex items-center gap-4">
-        <Image
-          src={productVariantImageUrl}
-          alt={productVariantName}
-          width={78}
-          height={78}
-          className="rounded-lg"
-        />
-        <div className="flex flex-col gap-1">
-          <p className="text-sm font-semibold">{productName}</p>
-          <p className="text-muted-foreground text-xs font-medium">
-            {productVariantName}
-          </p>
-          <div className="flex w-[100px] items-center justify-between rounded-lg border p-1">
-            <Button
-              className="h-4 w-4"
-              variant="ghost"
-              onClick={handleDecreaseQuantityClick}
-            >
-              <MinusIcon />
-            </Button>
-            <p className="text-xs font-medium">{quantity}</p>
-            <Button
-              className="h-4 w-4"
-              variant="ghost"
-              onClick={handleIncreaseQuantityClick}
-            >
-              <PlusIcon />
-            </Button>
+    <>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src={productVariantImageUrl}
+            alt={productVariantName}
+            width={78}
+            height={78}
+            className="rounded-lg"
+          />
+          <div className="flex flex-col gap-1">
+            <p className="text-sm font-semibold">{productName}</p>
+            <p className="text-muted-foreground text-xs font-medium">
+              {productVariantName}
+            </p>
+            <div className="flex w-[80px] items-center justify-between rounded-lg border p-1 md:w-[100px]">
+              <Button
+                className="h-4 w-4"
+                variant="ghost"
+                onClick={handleDecreaseQuantityClick}
+              >
+                <MinusIcon />
+              </Button>
+              <p className="text-xs font-medium">{quantity}</p>
+              <Button
+                className="h-4 w-4"
+                variant="ghost"
+                onClick={handleIncreaseQuantityClick}
+              >
+                <PlusIcon />
+              </Button>
+            </div>
           </div>
         </div>
+        <div className="flex flex-col items-end justify-center gap-2">
+          <Button
+            variant="outline"
+            size="icon"
+            onClick={handleDeleteClick}
+            className="cursor-pointer"
+          >
+            <TrashIcon />
+          </Button>
+          <p className="text-sm font-bold">
+            {formatCentsToBRL(productVariantPriceInCents)}
+          </p>
+        </div>
       </div>
-      <div className="flex flex-col items-end justify-center gap-2">
-        <Button variant="outline" size="icon" onClick={handleDeleteClick}>
-          <TrashIcon />
-        </Button>
-        <p className="text-sm font-bold">
-          {formatCentsToBRL(productVariantPriceInCents)}
-        </p>
-      </div>
-    </div>
+
+      <Dialog open={isDeleteDialogOpen} onOpenChange={setIsDeleteDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="md:text-lg">
+              Remover item do carrinho
+            </DialogTitle>
+            <DialogDescription className="md:text-sm">
+              Tem certeza que deseja remover este item do carrinho?
+            </DialogDescription>
+          </DialogHeader>
+
+          <DialogFooter>
+            <Button
+              variant="outline"
+              onClick={() => setIsDeleteDialogOpen(false)}
+              className="cursor-pointer"
+            >
+              Cancelar
+            </Button>
+            <Button
+              variant="default"
+              onClick={handleConfirmDelete}
+              className="cursor-pointer"
+            >
+              Remover
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
